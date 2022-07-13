22a214334037b7837cbe1112643c470b8f3998c4
evan bechtol <evanbechtol@gmail.com>
2022-07-13 15:02:12 -0500

    Update LandingPage.tsx

--- src/components/landingPage/LandingPage.tsx
+++ src/components/landingPage/LandingPage.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Button from '@mui/material/Button';
-import logo from '../../assets/img/logo.png';
+
 import './styles.css';
 
 const LandingPage = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="app-logo" alt="logo" />
         <p>Welcome to Wildridge Disc Golf!</p>
         <Link className="App-link" to="/new-round">
           <Button variant="contained">Start a round</Button>
