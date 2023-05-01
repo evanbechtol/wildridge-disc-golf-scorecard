2d0132166f996323be607cfb32db9d0dacd7d7b8
evan bechtol <evanbechtol@gmail.com>
2023-04-30 19:09:34 -0500

    update NewRound.tsx

--- src/components/NewRound/NewRound.tsx
+++ src/components/NewRound/NewRound.tsx
@@ -1,43 +1,66 @@
-import './styles.css';
-import * as React from 'react';
-import { GridColDef } from '@mui/x-data-grid';
-import DesktopLayout from './DesktopLayout/DesktopLayout';
-import MobileLayout from './MobileLayout/MobileLayout';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const NewRound = () => {
-  const columns: GridColDef[] = [
-    { field: 'id', headerName: 'Hole', width: 90 },
-    {
-      field: 'playerName',
-      headerName: 'player',
-      width: 150,
-      editable: true
-    },
-    {
-      field: 'score',
-      headerName: 'Score',
-      width: 150,
-      editable: true
-    }
-  ];
+  const [numberOfPlayers, setNumberOfPlayers] = useState(1);
 
-  const rows = [
-    { id: 1, playerName: 'Snow', score: 2 },
-    { id: 2, playerName: 'Lannister', score: 3 },
-    { id: 3, playerName: 'Ted', score: 1 },
-    { id: 4, playerName: 'Stark', score: 4 },
-    { id: 5, playerName: 'Targaryen', score: 4 },
-    { id: 6, playerName: 'Melisandre', score: 2 },
-    { id: 7, playerName: 'Clifford', score: 5 },
-    { id: 8, playerName: 'Frances', score: 4 },
-    { id: 9, playerName: 'Roxie', score: 2 }
-  ];
+  const handleIncrement = () => {
+    if (numberOfPlayers < 8) {
+      setNumberOfPlayers(numberOfPlayers + 1);
+    }
+  };
+
+  const handleDecrement = () => {
+    if (numberOfPlayers > 1) {
+      setNumberOfPlayers(numberOfPlayers - 1);
+    }
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    // validate the input value
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // create a new round object and store it in Recoil state
+  };
 
   return (
-    <>
-      <DesktopLayout rows={rows} columns={columns} />
-      <MobileLayout />
-    </>
+    <div>
+      <h1>New Round</h1>
+      <form onSubmit={handleSubmit}>
+        <button type="button" onClick={handleDecrement}>
+          -
+        </button>
+        <label htmlFor="numberOfPlayers">Number of Players</label>
+        <input
+          type="number"
+          id="numberOfPlayers"
+          name="numberOfPlayers"
+          min={1}
+          max={8}
+          value={numberOfPlayers}
+          onChange={handleInputChange}
+        />
+        <button type="button" onClick={handleIncrement}>
+          +
+        </button>
+
+        <br />
+        {Array.from({ length: numberOfPlayers }).map((_, index) => (
+          <div key={index}>
+            <h4>Player {index + 1} </h4>
+            <label htmlFor={`playerName-${index}`}>Name</label>
+            <input type="text" id={`playerName-${index}`} name={`playerName-${index}`} required />
+            <label htmlFor={`playerHandicap-${index}`}>Handicap</label>
+            <input type="number" id={`playerHandicap-${index}`} name={`playerHandicap-${index}`} />
+          </div>
+        ))}
+        <button type="submit">Start Round</button>
+      </form>
+      <Link to="/">Cancel</Link>
+    </div>
   );
 };
 
